5d13619500750b09f0f989d30364096fbbec6042
amiadtalmoncaravan8 <amiadtalmon@gmail.com>
2026-06-15 20:28:04 +0300

    Refactor data fetching and update comments

--- app.py
+++ app.py
@@ -10,17 +10,22 @@ st.set_page_config(page_title="Wagon System Pro", layout="wide")
 st.title("🏇 Wagon System: Diagnostic Protocol")
 
 if st.button("הרצת פרוטוקול אבחון"):
-    # 1. נתוני התיק (הקראוון)
-    data = yf.download(WAGON_TICKERS, period="1d", interval="1m")['Close'].iloc[-1]
+    # 1. נתוני התיק
+    data = yf.download(WAGON_TICKERS, period="1d")['Close'].iloc[-1]
     df = pd.DataFrame({'מחיר נוכחי': data})
-    
     st.subheader("מצב הסוסים")
     st.table(df)
     
-    # 2. נתוני מאקרו להשוואה
+    # 2. נתוני מאקרו - תיקון המשיכה
     st.subheader("מדדי מאקרו לאבחון")
-    macro_data = yf.download(list(MACRO_TICKERS.values()), period="1d")['Close'].iloc[-1]
-    macro_df = pd.DataFrame({'ערך': macro_data}, index=MACRO_TICKERS.keys())
+    macro_list = list(MACRO_TICKERS.values())
+    macro_data = yf.download(macro_list, period="1d")['Close'].iloc[-1]
+    
+    # יצירת טבלה תקינה
+    macro_df = pd.DataFrame({'ערך': macro_data})
     st.table(macro_df)
     
-    st.success("פרוטוקול אבחון הושלם.")
+    # 3. הכנה לניתוח Out-Riders
+    st.subheader("דוח אבחון חריגות")
+    # כאן בעתיד נשווה את תשואת התיק מול המדדים
+    st.write("המערכת ממתינה להגדרת משקולות התיק כדי להתריע על ביצועי חסר.")
